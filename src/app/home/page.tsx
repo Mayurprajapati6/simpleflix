143e6acf9bbb21f4f04c85f1d4d6328d32471486
Mayur Prajapati <mproyur685@gmail.com>
2025-06-24 10:42:37 +0530

    Implemented Navbar

---
+++ src/app/home/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+//import { MainBoard } from "@/components/molecules/MainBoard";
+//import { MovieCardList } from "@/components/molecules/MovieCardList";
+import { Navbar } from "@/components/molecules/Navbar";
+
+export default function Home() {
+    return (
+        <>
+            <Navbar />
+            {/*<MainBoard >*/}
+
+            {/*<MovieCardList >*/}
+        </>
+    )
+}
